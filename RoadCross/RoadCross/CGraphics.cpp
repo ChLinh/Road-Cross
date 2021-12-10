2945b19f6bd835e1a8d068cb53f66fceff5c41e7
Hi_myname_is_jeff <74254098+hi-myname-is-jeff@users.noreply.github.com>
2021-12-10 20:36:16 +0700

    set_console

--- RoadCross/RoadCross/CGraphics.cpp
+++ RoadCross/RoadCross/CGraphics.cpp
@@ -11,3 +11,20 @@ void GotoXY(int x, int y)
 	HANDLE ConsoleHanle = GetStdHandle(STD_OUTPUT_HANDLE);
 	SetConsoleCursorPosition(ConsoleHanle, coord);
 }
+
+void SetConsoleWindow(int width, int height)
+{
+	HWND consoleWindow = GetConsoleWindow();
+	LONG Style = GetWindowLong(consoleWindow, GWL_STYLE);
+	Style = Style & ~(WS_MAXIMIZEBOX) & ~(WS_THICKFRAME);
+	SetWindowLong(consoleWindow, GWL_STYLE, Style);
+	CONSOLE_CURSOR_INFO info;
+	HANDLE ConsoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+	info.dwSize = 100;
+	info.bVisible = FALSE;
+	SetConsoleCursorInfo(ConsoleHandle, &info);
+	COORD ConsoleBufferSize = { width, height };
+	SetConsoleScreenBufferSize(ConsoleHandle, ConsoleBufferSize);
+	SMALL_RECT Rect = { 0,0,ConsoleBufferSize.X - 1,ConsoleBufferSize.Y - 1 };
+	SetConsoleWindowInfo(ConsoleHandle, TRUE, &Rect);
+}
