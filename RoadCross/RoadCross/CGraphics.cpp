7c52cbb6e2fcaacfcd9d6fc7ff9eeef731b1a7e6
tonphuongoanh <oanh.phuongton@gmail.com>
2021-12-10 19:39:20 +0700

    GotoXY

--- RoadCross/RoadCross/CGraphics.cpp
+++ RoadCross/RoadCross/CGraphics.cpp
@@ -2,3 +2,12 @@
 #include "CGraphics.h"
 #include <fstream>
 using namespace std;
+
+void GotoXY(int x, int y)
+{
+	COORD coord;
+	coord.X = x;
+	coord.Y = y;
+	HANDLE ConsoleHanle = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleCursorPosition(ConsoleHanle, coord);
+}
