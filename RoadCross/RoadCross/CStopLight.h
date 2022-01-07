601a67cb938d3bfb19f7f2b975a8e8aa2b25558e
ChLinh <linh63021@gmail.com>
2022-01-07 23:51:32 +0700

    update stoplight

--- RoadCross/RoadCross/CStopLight.h
+++ RoadCross/RoadCross/CStopLight.h
@@ -2,15 +2,11 @@
 #include "CGraphics.h"
 #define Red 4000		 
 #define Green 3000	 
-#define TIME_BLINKING 2000				 
-#define ONE_SECOND 1000	
 class StopLight
 {
 private:
 	int countDown;
-	int countTime;
 	bool isRed;
-	bool warning, hidden;
 	int delay;
 	void SetTime(char);
 public:
